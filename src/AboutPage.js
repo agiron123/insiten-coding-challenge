189620ddb882a819d2279fa52c4dc90e20c3d4cd
Andre Giron <andre@beamlabs.net>
2018-12-17 23:54:49 -0500

    Adds react router to project

---
+++ src/AboutPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class AboutPage extends Component {
+  render() {
+    return (
+        <div>
+            <h2>Hello from AboutPage!</h2>
+        </div>
+    )
+  }
+}
+
+export default AboutPage;
